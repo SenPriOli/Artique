f6dd729cbe2c4c0dd2e3e990c41ab3cbb763e540
SenPriOli <sen.prioli@gmail.com>
2025-04-14 23:25:08 +0800

    Add files via upload

---
+++ loginSignUp/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Artique</title>
+    <link rel="stylesheet" href="loginStyle.css">
+</head>
+<body>
+    <div class="loginBox">
+        <h1>Login</h1>
+        <form action="">
+            <div class="inputBox">
+                <label for="username">Username</label>
+                <input type="text" name="username">
+            </div>
+            <div class="inputBox">
+                <label for="password">Password</label>
+                <input type="text" name="password">
+            </div>
+        </form>
+        <a href="">Sign Up</a>
+    </div>
+</body>
+</html>
