160a3fc505655b88daf342d5e89452baa835d584
SenPriOli <sen.prioli@gmail.com>
2025-04-14 22:13:51 +0800

    Create login.php
    login page

---
+++ loginSignUp/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Artique</title>
+</head>
+<body>
+    <div>
+        <h1>Login</h1>
+        <form action="">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="text" name="password">
+            </div>
+        </form>
+        <a href="">Sign Up</a>
+    </div>
+</body>
+</html>
